e2b4f5dadd07566aabc923b621b9bbfa99f1beae
Miguel <miguelangelcarrenopinto@gmail.com>
2025-07-30 15:03:54 -0500

    Arreglando

---
+++ POS5/conexion.php
@@ -0,0 +1,16 @@
+<?php
+function obtenerConexion() {
+    $host = "localhost";
+    $db = "posnova_bd";
+    $user = "root";
+    $pass = "";
+
+    try {
+        $conexion = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+        $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $conexion;
+    } catch (PDOException $e) {
+        die("Error de conexión: " . $e->getMessage());
+    }
+}
+?>
